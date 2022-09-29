0cf22abf9cb8c683eaf67cf00471dd1151569c5d
DmitryDemid <dimon.demid15@gmail.com>
2022-09-29 00:31:38 -0400

    Creating pages for payment opt

--- src/pages/CancellationPage/CancellationPage.tsx
+++ src/pages/CancellationPage/CancellationPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import s from './CancellationPage.module.css';
+import cancelIco from "../../assets/errorIco.svg";
 
 function CancellationPage() {
     return (
         <div className={s.CancellationPage}>
-            CancellationPage
+            <img src={cancelIco} alt="success" className={s.CancelIco}/>
+            <h2 className={s.CancelHeader}>Payment was canceled.</h2>
+            <p className={s.CancelInfo}>Try again or contact info@stylescan.com for help.</p>
+            <button className={s.CancelBtn}>Go back</button>
         </div>
     );
 }
--- src/pages/SuccessPage/SuccessPage.tsx
+++ src/pages/SuccessPage/SuccessPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import successIco from '../../assets/successIco.svg'
 import s from './SuccessPage.module.css';
 
 function SuccessPage() {
     return (
         <div className={s.SuccessPage}>
-            SuccessPage
+            <img src={successIco} alt="success" className={s.SuccessIco}/>
+            <h2 className={s.SuccessHeader}>Payment successful!</h2>
+            <p className={s.SuccessInfo}>Thanks for your purchase. Invoice has been sent to your email.</p>
+            <button className={s.SuccessBtn}>Go back</button>
         </div>
     );
 }
